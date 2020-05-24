fcbc63306ae6e8c3387ead9cce0de7617328eead
hazem-kamel <57861165+hazem-kamel@users.noreply.github.com>
2020-05-24 22:05:28 +0200

    Deploying

---
+++ client/build/precache-manifest.a6c3b91232ad649ca5d69b29bfb5f816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663a9c4f83c9f40dde00f65473691680",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c38a601794b82d6c693",
+    "url": "/static/css/main.3d58b3ca.chunk.css"
+  },
+  {
+    "revision": "e1c8e17c8d2efb903fa6",
+    "url": "/static/js/2.dacfba7b.chunk.js"
+  },
+  {
+    "revision": "962ff5be0404fbe55043f4e01f539a83",
+    "url": "/static/js/2.dacfba7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c38a601794b82d6c693",
+    "url": "/static/js/main.42a15d6f.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
